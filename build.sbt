4002b5023e96abc3c3048c395ce27f973ca72f72
Chia-Chuan Wu <chia.chuan.wu@sv.cmu.edu>
2015-12-30 15:00:54 +0800

    first round map reduce will need to work on second round

--- build.sbt
+++ build.sbt
@@ -4,6 +4,9 @@ version := "1.0"
 
 scalaVersion := "2.11.7"
 
-libraryDependencies += Seq("org.apache.spark" %% "spark-core" % "1.5.2"
-,"org.apache.hadoop" % "hadoop-client" % "2.4.0")
+libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % "1.5.2",
+  "org.apache.hadoop" % "hadoop-client" % "2.4.0",
+  "org.apache.spark" % "spark-streaming_2.10" % "1.5.2"
+)
     
---
+++ src/main/java/Preprocess.java
@@ -0,0 +1,57 @@
+import org.apache.commons.collections.map.HashedMap;
+
+import java.io.*;
+import java.util.Map;
+
+/**
+ * Created by bluebyte60 on 12/29/15.
+ */
+public class Preprocess {
+
+    public static Map<Integer, String> getMap(String line) {
+        Map<Integer, String> map = new HashedMap();
+        String[] words = line.split(",");
+        for (int i = 0; i < words.length; i++) {
+            map.put(i, words[i]);
+        }
+        return map;
+    }
+
+    public static void listFilesForFolder(final File folder) {
+        for (final File fileEntry : folder.listFiles()) {
+            if (fileEntry.isDirectory()) {
+                listFilesForFolder(fileEntry);
+            } else {
+                System.out.println(fileEntry.getName());
+                parse(fileEntry);
+            }
+        }
+    }
+
+    public static void parse(File fileEntry) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileEntry));
+            String cat = fileEntry.getName().split("_")[1];
+            PrintWriter writer = new PrintWriter(cat + ".parsed", "UTF-8");
+            String line = br.readLine();
+            Map<Integer, String> map = getMap(line);
+            while ((line = br.readLine()) != null) {
+                writer.print(cat);
+                String[] tokons = line.split(",");
+                for (int i = 0; i < tokons.length; i++) {
+                    for (int j = 0; j < Integer.parseInt(tokons[i]); j++)
+                        writer.print(" " + map.get(i));
+                }
+                writer.println();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        listFilesForFolder(new File("data"));
+    }
+}
