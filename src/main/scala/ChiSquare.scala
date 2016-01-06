d1e798d7dc5c3e3b35c4fd6807ba5b50b6902391
Chia-Chuan Wu <chia.chuan.wu@sv.cmu.edu>
2016-01-06 22:05:33 +0800

    Update Chi-square function  into correct version, make code scalable without memory restriction

--- src/main/scala/ChiSquare.scala
+++ src/main/scala/ChiSquare.scala
@@ -1,60 +1,84 @@
-import java.io.{PrintWriter, File}
+import org.apache.spark.{SparkConf, SparkContext}
 
-import org.apache.spark.rdd.RDD
-import org.apache.spark.streaming.{StreamingContext, Seconds}
-import org.apache.spark.{SparkContext, SparkConf}
-
+import scala.collection.immutable.HashSet
 import scala.collection.mutable.ListBuffer
+import scala.collection.parallel.mutable
+import scala.reflect.io.File
 
 /**
  * Created by bluebyte60 on 12/29/15.
+ * This chi-square implementation returns a ranked list of features with corresponding chi-square value
  */
 object ChiSquare {
 
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf().setAppName("chisquare").setMaster("local[2]")
     val sc = new SparkContext(conf)
-    val inputDir = sc.wholeTextFiles("data/parsed/")
-    val data = inputDir.flatMap { case (filename, content) => content.split("\n") }.map(line => {
-      val tokons: List[String] = line.split(" ").toList
+    //0. Get stopword
+    val stopf = sc.wholeTextFiles("data/stopwords/").flatMap { case (filename, content) => content.split("\r\n") }.collect().toList
+    val stopwords = sc.broadcast(HashSet() ++ stopf).value
+
+    //1. Tokonization
+    val inputDir = sc.textFile("data/reuters/train")
+    val data = inputDir.map(line => {
+      var tokons: List[String] = List("")
+      for (term <- line.split("\\s+")) {
+        val t = term.trim
+        if (t.length > 2 && !stopwords.contains(t))
+          tokons = tokons :+ t
+      }
       tokons
     }).cache()
 
-    //1. Get total frequency map
-    val Total = data.flatMap(tokon => {
+    //2. Get total frequency
+    val N = data.flatMap(tokon => {
       tokon.map(x => "N")
-    }).map(word => (word, 1)).reduceByKey(_ + _, 1).cache()
-    //2. Get frequency map given a term
+    }).map(word => (word, 1)).reduceByKey(_ + _, 1).values.collect()(0)
+
+    //3. Get frequency map given a term, (key: term, value: frequency)
     val T = data.flatMap(tokons => {
       tokons.map(x => (x, 1))
-    }).collectAsMap()
-    //3. Get frequency given a category
-    val C = data.map(tokons => tokons(0)).map(word => (word, 1)).reduceByKey(_ + _, 1).cache().collectAsMap()
-    //4. Get term frequency map given a term and a category
+    }).reduceByKey(_ + _, 1).filter { case (k, v) => v >= 5 }
+
+    //4. Get frequency map given a category, (key:category, value: frequency)
+    val C = data.map(tokons => tokons(0)).map(cat => (cat, 1)).reduceByKey(_ + _, 1).cache().collectAsMap()
+
+    //5. Get term frequency map given a term and a category, (key:term, value: Map(category, frequency))
     val T_C = data.flatMap(tokons => {
       //count term frequency(a+c) & category frequency (a+b)
       var seq = new ListBuffer[String]();
-      for (a <- 1 until tokons.length) {
-        seq = seq :+ tokons(0) + "_" + tokons(a)
+      for (i <- 1 until tokons.length) {
+        seq = seq :+ tokons(0) + File.separator + tokons(i) //cat_term, 1
       }
       seq
     }).map(word => (word, 1)).reduceByKey(_ + _, 1)
-    //5. Start to calculate Chi-square
-    val N = Total.values.collect()(0)
+      .map { case (k, v) => (k.split(File.separator)(1), k.split(File.separator)(0) + File.separator + v) } //term, cat_fre
+      .aggregateByKey(mutable.ParHashMap.empty[String, Int])(addToMap, mergePartitionMaps)
+    //6. Start to calculate Chi-square
     val V = T.keys
     val categories = C.keys
-    val r = T_C.flatMap { case (key: String, value: Int) => {
-      var seq = new ListBuffer[String]();
-      val cat = key.split("_")(0)
-      val term = key.split("_")(1)
-      val a = value
-      val a_b = C.get(cat).get
-      val a_c = T.get(term).get
-      seq = seq :+ (cat + "_" + term + ":" + chi(a, a_b, a_c, N))
+    val combined = T.join(T_C)
+    val r = combined.flatMap { case (term: String, pair: Tuple2[Int, mutable.ParHashMap[String, Int]]) => {
+      var seq = new ListBuffer[Tuple2[String, Double]]();
+      val freqs = pair._2
+      for (c <- categories) {
+        var a = freqs.getOrElse(c, 0)
+        val a_b = C.get(c).get
+        val a_c = pair._1
+        seq = seq :+ Tuple2(term, chi(a, a_b, a_c, N))
+      }
       seq
     }
-    }.map(word => (word.split(":")(0), word.split(":")(1).toDouble)).reduceByKey(_ + _, 1).
-      map(item => item.swap).sortByKey(true, 1).map(item => item.swap).saveAsTextFile("Chisquare_result")
+    }.reduceByKey(_ + _, 1).map(item => item.swap).sortByKey(true, 1).map(item => item.swap).saveAsTextFile("Chisquare_result")
+  }
+
+  def addToMap(s: mutable.ParHashMap[String, Int], v: String): mutable.ParHashMap[String, Int] = {
+    s.put(v.split(File.separator)(0), v.split(File.separator)(1).toInt)
+    s
+  }
+
+  def mergePartitionMaps(p1: mutable.ParHashMap[String, Int], p2: mutable.ParHashMap[String, Int]) = {
+    p1 ++ p2
   }
 
   def chi(a: Int, a_b: Int, a_c: Int, N: Int): Double = {
@@ -63,6 +87,6 @@ object ChiSquare {
     val b_d = N - a_c
     val d = b_d - b;
     val c_d = N - a_b;
-    return (N * Math.pow(a.toFloat * d.toFloat - b.toFloat * c.toFloat, 2)) / ((a_b.toFloat) * c_d.toFloat * a_c.toFloat * b_d.toFloat)
+    return (N.toFloat * Math.pow(a.toFloat * d.toFloat - b.toFloat * c.toFloat, 2)) / ((a_b.toFloat) * c_d.toFloat * a_c.toFloat * b_d.toFloat)
   }
 }
