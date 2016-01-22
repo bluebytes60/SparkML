daa6cd5a2a8a5c4b5d0b5318514780f4278e25fe
Chia-Chuan Wu <chia.chuan.wu@sv.cmu.edu>
2016-01-23 05:33:05 +0800

    add offline SVM training on avid dataset

---
+++ src/main/scala/avito/linearSVM.scala
@@ -0,0 +1,93 @@
+package avito
+
+import org.apache.spark.mllib.classification.{SVMModel, SVMWithSGD}
+import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
+import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.regression.{LabeledPoint, StreamingLinearRegressionWithSGD}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{Row, SQLContext}
+import org.apache.spark.streaming.{Milliseconds, StreamingContext}
+
+import scala.collection.mutable
+
+/**
+  * Created by bluebyte60 on 1/22/16.
+  */
+object linearSVM {
+
+  def main(args: Array[String]) {
+    val dbUrl = "jdbc:sqlite:/Users/bluebyte60/Documents/github/SparkML/data/avito/database.sqlite"
+
+    val table = "train"
+
+    val driver = "org.sqlite.JDBC"
+
+    val features = 4
+
+    val conf = new SparkConf().setAppName("QueueStream").setMaster("local[2]")
+
+    val sc = new SparkContext(conf)
+
+    val sqlContext = new SQLContext(sc)
+
+    val jdbcDF = sqlContext.read.format("jdbc").options(
+      Map("url" -> dbUrl, "dbtable" -> table)).load()
+
+    val data = jdbcDF.map(row => LabeledPoint.parse(toString(row)))
+
+    val splits = data.randomSplit(Array(0.6, 0.4), seed = 11L)
+
+    val training = splits(0).cache()
+
+    val test = splits(1)
+
+
+    // Run training algorithm to build the model
+    val numIterations = 10000
+    val model = SVMWithSGD.train(training, numIterations)
+
+    // Clear the default threshold.
+    model.clearThreshold()
+
+    // Compute raw scores on the test set.
+    val scoreAndLabels = test.map { point =>
+      val score = model.predict(point.features)
+      (score, point.label)
+    }
+
+    // Get evaluation metrics.
+    val metrics = new BinaryClassificationMetrics(scoreAndLabels)
+
+    // Precision by threshold
+    //    val precision = metrics.precisionByThreshold
+    //    precision.foreach { case (t, p) =>
+    //      println(s"Threshold: $t, Precision: $p")
+    //    }
+
+    // Recall by threshold
+    //    val recall = metrics.recallByThreshold
+    //    recall.foreach { case (t, r) =>
+    //      println(s"Threshold: $t, Recall: $r")
+    //    }
+
+    // F-measure
+    val f1Score = metrics.fMeasureByThreshold
+    f1Score.foreach { case (t, f) =>
+      println(s"Threshold: $t, F-score: $f, Beta = 1")
+    }
+
+    //    // Save and load model
+    //    model.save(sc, "myModelPath")
+    //    val sameModel = SVMModel.load(sc, "myModelPath")
+
+  }
+
+  def toString(row: Row): String = {
+    var features = new Array[Any](row.length - 1)
+    for (idx <- 1 to row.length - 1) features(idx - 1) = String.valueOf(row(idx))
+    String.format("(%s, [%s])", row.getLong(0).toString,
+      features mkString ",")
+  }
+
+}
